2ef49497e128b1cefe62b8b922934973091ec6f5
Aryan Pal <aryanpal401@gmail.com>
2025-07-14 18:00:43 +0530

    Added contact button in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -24,6 +24,7 @@ const Footer = () => {
             // { name: "Experience", id: "experience" },
             { name: "Projects", id: "work" },
             { name: "Education", id: "education" },
+            { name: "Contact", id: "contact" },
           ].map((item, index) => (
             <button
               key={index}
